082668bb1d6f7d7382737a885bbe393804d88790
MatheusPhillip <matheus-phillip@hotmail.com>
2020-07-26 14:24:02 -0300

    Ajustes na classe Main.

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -6,8 +6,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		// INICIA O PARSER NO SEU CONSTRUTOR
 		GameController gameController = new GameController();
+		
+		// PROCURA UM GAME POR ID
 		gameController.getGameById("game_2");
+		
+		// MOSTRA TODOS OS JOGOS COM RANKING DE KILLS POR JOGADOR
 		gameController.showAllGamesScore();
 	}
 
